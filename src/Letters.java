39bf826cf65833f173d90393f6fc1e5986cc007e
syeraolg000 <syeraolg000@CAM-LAB-29.bgsd.k12.wa.us>
2019-10-01 06:56:08 -0700

    Finished

--- src/Letters.java
+++ src/Letters.java
@@ -3,7 +3,52 @@ public class Letters {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+        System.out.println("Dear Parents,");
+        letter();
+        System.out.println();
+        System.out.println("A programmer had a problem.");
+        System.out.println("He decided to use Java.");
+        System.out.println("He now has a ProblemFactory.");
+        System.out.println();
+        System.out.println("This is what a programers life revolves around.");
+        System.out.println("I can't fix this.");
+        System.out.println("Crisis of confidence.");
+        System.out.println("Questions career");
+        System.out.println("Questions life");
+        System.out.println("Oh it was a type");
+        System.out.println();
+        System.out.println("Dear Masha,");
+        letter();
+        System.out.println();
+        System.out.println("Programmer's son asks his father:");
+        System.out.println("Dad, why do the sun rise on the east");
+        System.out.println("an set on the west?");
+        System.out.println("Father: It works? don't touch it.");
+        System.out.println();
+        System.out.println("\" Stay away from negative people");
+        System.out.println("They have a problem for every solution.\"");
+        System.out.println("-Albert Einstein");
+        System.out.println();
+        System.out.println("Dear Suzy");
+        letter();
+        System.out.println();
+        System.out.println("My code never has bugs");
+        System.out.println("it just developes");
+        System.out.println("random unexpected features.");
+        System.out.println();
+        System.out.println("The reason why programmers");
+        System.out.println("don't like nature");
+        System.out.println("is because there\'s to many bugs");
+       
+	}
+	public static void letter() {
+		System.out.println("Hello,");
+		System.out.println("I've started a programming class in school.");
+		System.out.println("It starts on a hour earlier than my other classes");
+		System.out.println("and we are learning the Java language.");
+		System.out.println("Here are some jokes about programming.");
+		
+        
 	}
 
 }
